ec34c77370376cf660e187187e4f0264f6308a7b
TuAmor-XD <2021256010@ub.edu.bz>
2026-02-12 18:38:41 -0600

    xd

--- dist/db/dbconnection.js
+++ dist/db/dbconnection.js
@@ -5,7 +5,7 @@ const pool = new pg_1.Pool({
     user: "postgres",
     host: "localhost",
     database: "movierentaldb",
-    password: "P@ssword1", // change to your password
+    password: "5502", // change to your password
     port: 5432
 });
 exports.default = pool;
